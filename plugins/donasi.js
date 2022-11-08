b10720534ceb189dc64836b36bda0c2a333279c1
Kiyotakakin <kytokin70@gmail.com>
2022-11-09 00:42:04 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,10 +2,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 「 Suport Bot Dengan Cara donasi 」
 
-⫹⫺ Via Pulsa Indosat : 6285850539404
-⫹⫺ Via Dana : 6285850539404
-⫹⫺ Via Saweria :
-⫹⫺ https://saweria.co/jarotoffc
+⫹⫺ Via Pulsa : 083172999950
+⫹⫺ Via Dana : 083172999950
 
 `.trim()
   m.reply(text)
